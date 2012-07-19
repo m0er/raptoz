53645ae0c2634be189c3ffe883accf12d501e929
Yunseok Choi <ethdemor@gmail.com>
2012-07-19 15:33:05 +0900

    import 정리.

--- test/com/raptoz/tag/TagServiceTest.java
+++ test/com/raptoz/tag/TagServiceTest.java
@@ -11,7 +11,6 @@ import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
