6b92d57be6da701ad447322ca1773b3681987d26
Yunseok Choi <ethdemor@gmail.com>
2012-10-02 00:32:14 +0900

    Ember.js 제거.

--- web/js/ember/load.js
+++
@@ -1,3 +0,0 @@
-define(['ember/ember'], function() {
-	return Ember;
-});
--- web/js/ember/models/User.js
+++
@@ -1,9 +0,0 @@
-define(function() {
-	return Em.Object.extend({
-		id: null,
-		joined: null,
-		email: null,
-		nickname: null,
-		encodeProfileImage: null
-	});
-});
