7cbe18e8b1c46a067d5060d59a7a19333ab8ef3d
Yunseok Choi <ethdemor@gmail.com>
2012-10-14 22:19:47 +0900

    Spring extention - 관련 클래스 패키지 이동

--- src/com/raptoz/spring/converter/ObjectIdToUserConverter.java
+++ src/com/raptoz/spring/converter/ObjectIdToUserConverter.java
@@ -1,27 +1,25 @@
-package com.raptoz.util;
+package com.raptoz.spring.converter;
 
 import java.lang.annotation.Annotation;
 import java.util.Collections;
 import java.util.Set;
 
 import org.bson.types.ObjectId;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.core.convert.converter.ConditionalGenericConverter;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.raptoz.user.User;
 import com.raptoz.user.UserRepository;
 
 public class ObjectIdToUserConverter implements ConditionalGenericConverter {
 	@Autowired UserRepository userRepository;
 	
-	private Logger logger = LoggerFactory.getLogger(getClass());
-	
 	@Override
 	public Set<ConvertiblePair> getConvertibleTypes() {
-		return Collections.singleton(new ConvertiblePair(Object.class, Object.class));
+		return Collections.singleton(new ConvertiblePair(String.class, User.class));
 	}
 
 	@Override
@@ -31,12 +29,9 @@ public class ObjectIdToUserConverter implements ConditionalGenericConverter {
 
 	@Override
 	public boolean matches(TypeDescriptor sourceType, TypeDescriptor targetType) {
-		logger.info(sourceType.toString());
-		logger.info(targetType.toString());
-		
 		Annotation pathVariable = targetType.getAnnotation(PathVariable.class);
 		
-		if (pathVariable != null && ((PathVariable) targetType.getAnnotation(PathVariable.class)).value().toLowerCase().contains("id"))
+		if (pathVariable != null && targetType.getType().isAnnotationPresent(Document.class))
 			return true;
 		
 		return false;
