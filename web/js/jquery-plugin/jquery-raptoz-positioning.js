8d34507cb18f66768f77467be04972c90389be56
Yunseok Choi <ethdemor@gmail.com>
2012-12-20 01:22:39 +0900

    jQuery-raptoz-positioning - 주석 수정 및 리팩토링

--- web/js/jquery-plugin/jquery-raptoz-positioning.js
+++ web/js/jquery-plugin/jquery-raptoz-positioning.js
@@ -10,14 +10,9 @@ define(['jquery', 'jqueryui/load'], function (jQuery) {
  *   );
  * 
  * Parameters(type: "pinterest"):
- *   column: 컨텐츠를 몇 줄로 배치할지 결정합니다. 디폴트는 2줄 입니다.
+ *   column: 컨텐츠를 한 행에 몇 개씩 배치할지 결정합니다. 디폴트는 한 행에 두 개의 엘리먼트 입니다.
  *   x: 엘리먼트 간의 가로 간격을 지정합니다. 디폴트는 "10 0"
  *   y: 엘리먼트 간의 세로 간격을 지정합니다. 디폴트는 "0 10"
- * 
- * Parameters(type: "list"):
- *   x:
- *   y:
- *   
  * 
  * Depends:
  *   jquery.js
@@ -36,6 +31,11 @@ define(['jquery', 'jqueryui/load'], function (jQuery) {
 			var $row = this.slice(0, 1);
 			var $contents = this.slice(1, this.length);
 			
+			// 원하는 컬럼 수 보다 엘리먼트가 적을 때
+			if ($contents.size() < opts.column) {
+				opts.column = $contents.size();
+			}
+			
 			pinterestPositioning($row, $contents, opts);
 			
 			$(opts.wrapper).css("visibility", "visible");
@@ -45,18 +45,27 @@ define(['jquery', 'jqueryui/load'], function (jQuery) {
 	function pinterestPositioning($row, $contents, opts) {
 		$contents.eq(0).position({of: $row, at: "left top", collision: "none", my: "left top"});
 		
-		// 원하는 컬럼 수 보다 엘리먼트가 적을 때
-		if ($contents.size() < opts.column) {
-			opts.column = $contents.size();
+		// 기준이 되는 컨텐츠 부터 배치
+		for (var i = 0; i < opts.column; i++) {
+			$contents.eq(i + 1).position({
+				of: $contents.eq(i), 
+				at: "right top", 
+				collision: "none", 
+				my: "left top", 
+				offset: opts.x
+			});
 		}
 		
-		// 기준이 되는 컨텐츠 부터 배치
-		for (var i = 0; i < opts.column; i++)
-			$contents.eq(i + 1).position({of: $contents.eq(i), at: "right top", collision: "none", my: "left top", offset: opts.x});
-		
 		// 나머지 컨텐츠를 기준 컨텐츠 밑으로 정렬시킴
-		for (var i = 0, j = opts.column; i < $contents.size() - opts.column; i++, j++)
-			$contents.eq(j).position({of: $contents.eq(i), at: "bottom", collision: "none", my: "top", offset: opts.y});
+		for (var i = 0, j = opts.column; i < $contents.size() - opts.column; i++, j++) {
+			$contents.eq(j).position({
+				of: $contents.eq(i), 
+				at: "bottom", 
+				collision: "none", 
+				my: "top", 
+				offset: opts.y
+			});
+		}
 	}
 	
 	$.fn.positioning.defaults = {
